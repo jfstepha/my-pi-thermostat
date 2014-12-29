7ae542c13d3bcbf9d922d41e93a6a4b5235e8b44
Jon Stephan <jfstepha@gmail.com>
2014-12-29 10:31:10 -0500

    Added comments to startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # change the autologin user in /etc/lightdm/lightdm.conf
+# add this to .config/autostart/.desktop:
+#[Desktop Entry]
+#Type=Application
+#Exec=/home/jfstepha/bin/my-pi-thermostat/startup.sh
+
+
 cd /home/jfstepha/bin/my-pi-thermostat
 DISPLAY=:0.0 ./my_gui.py &
 sudo sh -c './rubustat_daemon.py start > logs/daemonlog_`date +%F`.log 2>&1'
