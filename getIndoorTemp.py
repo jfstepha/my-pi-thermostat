316ffe1b13bb9092f7cd773d2077155ec079dfc0
Jon Stephan <jfstepha@gmail.com>
2014-12-30 18:16:41 -0500

    Added some config options to make it more portable.

--- getIndoorTemp.py
+++ getIndoorTemp.py
@@ -7,11 +7,18 @@ import glob
 import time
 import urllib2
 import json
+import ConfigParser
+
+config = ConfigParser.ConfigParser()
+config.read("config.txt")
+servername = config.get('sensorhub','name')
+serverdomain = config.get('sensorhub','domain')
+serverishub = int(config.get('sensorhub', 'ishub'))
 
 def getIndoorTemp():
-    jsonurl = urllib2.urlopen("http://localhost/_liveSensorValue/sum_LR/temp")
+    jsonurl = urllib2.urlopen("http://localhost/_liveSensorValue/sum_"+serverdomain+"/temp")
     return float(json.loads(jsonurl.read()))
 
 def getIndoorTemp2():
-    jsonurl = urllib2.urlopen("http://localhost/_liveSensorValue/thermo_LR/temp")
+    jsonurl = urllib2.urlopen("http://localhost/_liveSensorValue/"+servername+"/temp")
     return float(json.loads(jsonurl.read()))
