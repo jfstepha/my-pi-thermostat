0239c29ee9781a7e4ccfba8abdbd7613e374a58a
Jon Stephan <jfstepha@gmail.com>
2016-11-15 19:16:55 -0500

    Added code to help on reboot

---
+++ pinger.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+curl http://localhost > logs/weblog1.html
+sleep 10
+curl http://localhost > logs/weblog2.html
+sleep 10
+curl http://localhost > logs/weblog3.html
+sleep 10
+curl http://localhost > logs/weblog4.html
+sleep 10
+curl http://localhost > logs/weblog5.html
+sleep 10
--- startup.sh
+++ startup.sh
@@ -30,4 +30,5 @@
 cd /home/jfstepha/bin/my-pi-thermostat
 DISPLAY=:0.0 ./my_gui.py &
 sudo sh -c './rubustat_daemon.py start > logs/daemonlog_`date +%F`.log 2>&1'
+sh -c ./pinger.sh &
 sudo sh -c './rubustat_web_interface.py > logs/log`date +%F`.log 2>&1'
