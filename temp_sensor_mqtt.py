2a01d5bb0e5d7817fd673fd79ea26d7af18e2f36
= <=>
2021-08-18 21:41:49 -0400

    Added the temp_sensor_homie.py Temp_sensor_homie calls an external python script to get the temperature readings because mqtt needs python3, but I couldn't upgrade the

--- temp_sensor_mqtt.py
+++ temp_sensor_mqtt.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python3
-# adafruit librarys need to be installed from PIP now:
-# pip3 install adafruit-circuitpython-dht
-# sudo apt-get install libgpiod2
+#!/usr/bin/python
+
+# for the old pi-based thermostats, for some reason, i can't get the Adafruit libraries to install
+# I can't get anything to install with PIP on python3
+# so i have to stick with the old library.
 
 # and the mqtt library:
-# sudo pip3 install paho-mqtt
+# sudo pip install paho-mqtt
 
 # create the startup file
 # /etc/systemd/system/temp_sensor_mqtt.service
@@ -12,16 +13,11 @@
 # start with sudo systemctl start temp_sensor_mqtt
 
 import sys
-import argparse
 import time
-import board
-import adafruit_dht
+import os
 
 import paho.mqtt.publish as pub
 from datetime import datetime
-
-parser = argparse.ArgumentParser(description='Read the power values from the pzem power meter')
-args = parser.parse_args()
 
 result = None
 retry = 0
@@ -29,18 +25,18 @@ retry = 0
 # MQTT settings
 MQTT_HOST="basement-pc"
 MQTT_PORT=1883
-MQTT_TOPIC="lexi-thermo/"
+MQTT_TOPIC="tate-thermo/"
 DEBUG=False
 loop=0
 
 # adafruit stuff 
-dhtDevice = adafruit_dht.DHT22(board.D4)
+#dhtDevice = adafruit_dht.DHT22(board.D4)
 
 def publish_value(reg_name, reg_val, loop_no):
     dt_string = now.strftime("%m/%d/%Y %H:%M:%S")
     topic = MQTT_TOPIC + reg_name
     if DEBUG or loop_no < 2:
-        print("[" + dt_string + "] pzem: Publishing MQTT topic {} value {} on loop {}".format(topic, reg_val, loop_no ) )
+        print("[" + dt_string + "] temp_sensor_mqtt: Publishing MQTT topic {} value {} on loop {}".format(topic, reg_val, loop_no ) )
         print("topic=" + topic)
     pub.single(topic , "{0:.1f}".format(reg_val), qos=2, hostname=MQTT_HOST, port=MQTT_PORT, client_id="sdm630_mqtt")
 
@@ -52,30 +48,43 @@ while 1:
   while result == None and retry < 5:
 
     retry += 1
-    try:
-#    if True:
-        temperature_c = dhtDevice.temperature
-        temperature_f = temperature_c * (9 / 5) + 32
-        humidity = dhtDevice.humidity
-        print(
-            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
-                temperature_f, temperature_c, humidity
+#    try:
+    if True:
+        stream = os.popen('./read_dht22.py')
+        dht_str = stream.read()
+        print("Got DHT string: " + dht_str);
+        # string should be "temp:74.120,rh:41.500"
+        if dht_str.find("temp:") == 0:
+            print("Found temp");
+            ary = dht_str.split(",");
+            temp_part = ary[0]
+            rh_part = ary[1]
+            ary = temp_part.split(":");
+            temperature_f = float( ary[1] );
+            art = temp_part.split(":");
+            humidity = float( ary[1] );
+            print("Temperature=%0.3f, RH=%0.3f" % (temperature_f, humidity) )
+            print(
+                "Temp: {:.1f} F /     Humidity: {}% ".format(
+                    temperature_f, humidity
+                )
             )
-        )
-        now=datetime.today()
-        dt_string = now.strftime("%m/%d/%Y %H:%M:%S")
-        print("[" + dt_string + "] thermo: publishing topics loop # " + str(loop) );
-        if DEBUG or loop < 2:
-            print("[" + dt_string + "] thermo: publishing topic " + MQTT_TOPIC + "lastdatasent " + dt_string)
-        pub.single(MQTT_TOPIC + "lastdatasent" , dt_string, qos=2, hostname=MQTT_HOST, port=MQTT_PORT, client_id="sdm630_mqtt")
+            now=datetime.today()
+            dt_string = now.strftime("%m/%d/%Y %H:%M:%S")
+            print("[" + dt_string + "] thermo: publishing topics loop # " + str(loop) );
+            if DEBUG or loop < 2:
+                print("[" + dt_string + "] thermo: publishing topic " + MQTT_TOPIC + "lastdatasent " + dt_string)
+            pub.single(MQTT_TOPIC + "lastdatasent" , dt_string, qos=2, hostname=MQTT_HOST, port=MQTT_PORT, client_id="sdm630_mqtt")
 
-        publish_value("humidity", humidity, loop);
-        publish_value("temperature", temperature_f, loop);
+            publish_value("humidity", humidity, loop);
+            publish_value("temperature", temperature_f, loop);
 
-        result = 1
-        time.sleep(30)
-#    else:   
-    except:
+            result = 1
+            time.sleep(10)
+        else:
+            print("failed to get reading, trying again");
+    else:   
+#    except:
         print('Failed to get reading. Try again!')
 
         print("Unexpected error:", sys.exc_info()[0])
---
+++ temp_sensor_mqtt_homie.py
@@ -0,0 +1,150 @@
+#!/usr/bin/python
+
+# for the old pi-based thermostats, for some reason, i can't get the Adafruit libraries to install
+# I can't get anything to install with PIP on python3
+# so i have to stick with the old library.
+
+# and the mqtt library:
+# sudo pip install paho-mqtt
+
+# create the startup file
+# /etc/systemd/system/temp_sensor_mqtt_homie.service
+
+# start with sudo systemctl start temp_sensor_mqtt
+
+import sys
+import time
+import os
+
+import paho.mqtt.publish as pub
+from datetime import datetime
+
+result = None
+retry = 0
+
+# MQTT settings
+MQTT_HOST="basement-pc"
+MQTT_PORT=1883
+did="tate-thermo"
+DEBUG=False
+loop=0
+
+# topics
+# /homie/did/$homie = 4.0
+# /homie/did/$name = did
+# /homie/did/$state = init  (then ready) (need to set LWT to "lost")
+# /homie/did/$nodes = "temperature,humidity
+# for prop = [temperature,humidity]:
+# /homie/did/prop/$name = prop
+# /homie/did/prop/$datatype = float
+# /homie/did/prop/$format = "-30:130" or "0:100"
+# /homie/did/prop/$settable = false
+# /homie/did/prop/$retained = true
+# /homie/did/prop = value
+
+
+
+# adafruit stuff 
+#dhtDevice = adafruit_dht.DHT22(board.D4)
+
+def publish_raw(topic_sub, msg, printit, will=None):
+    now=datetime.today()
+    dt_string = now.strftime("%m/%d/%Y %H:%M:%S")
+    topic = "homie/"+did+"/"+topic_sub
+    if printit:
+        print("[" + dt_string + "] temp_sensor_homie: Publishing MQTT topic {} value {} ".format(topic, msg ) )
+    pub.single(topic , msg, qos=2, hostname=MQTT_HOST, port=MQTT_PORT, retain=True, will=will)
+
+
+degree_sign = u'\N{DEGREE SIGN}'.encode('utf-8')
+
+will={'topic':"homie/"+did+"/$state", 'payload':'lost', 'qos':2,'retain':True}
+publish_raw("$homie","3.0.1", True);
+publish_raw("$name", did, True);
+#publish_raw("$localip", "192.168.1.105", True);
+#publish_raw("$mac", "00:13:ef:c0:17:4f", True);
+publish_raw("$fw-name", "temp_sensor_mqtt_homie.py", True);
+publish_raw("$fw-version", "0.0.0", True);
+publish_raw("$nodes", "thermostat", True);
+publish_raw("thermostat/$name", "thermostat", True);
+publish_raw("thermostat/$type", "PiB", True);
+publish_raw("thermostat/$properties", "temperature,humidity", True);
+publish_raw("thermostat/temperature/$name", "temperature", True);
+publish_raw("thermostat/temperature/$datatype", "float", True);
+publish_raw("thermostat/temperature/$format", "-30:130", True);
+publish_raw("thermostat/temperature/$unit", degree_sign+"F", True);
+publish_raw("thermostat/temperature/$settable", "false", True);
+publish_raw("thermostat/temperature/$retained", "true", True);
+publish_raw("thermostat/humidity/$name", "humidity", True);
+publish_raw("thermostat/humidity/$unit", "%", True);
+publish_raw("thermostat/humidity/$datatype", "float", True);
+publish_raw("thermostat/humidity/$format", "-30:130", True);
+publish_raw("thermostat/humidity/$settable", "false", True);
+publish_raw("thermostat/humidity/$retained", "true", True);
+
+publish_raw("$state","init",True,will);
+publish_raw("$stats","lastupdate,loop",True,will);
+
+#time.sleep(10)
+        
+while 1:
+  result = None
+  loop += 1
+  retry = 1
+
+  while result == None and retry < 5:
+
+    retry += 1
+#    try:
+    if True:
+        stream = os.popen('./read_dht22.py')
+        dht_str = stream.read()
+        #print("Got DHT string: " + dht_str);
+        # string should be "temp:74.120,rh:41.500"
+        if dht_str.find("temp:") == 0:
+            if loop < 2:
+                printit = True
+            else:
+                printit = False
+            #print("Found temp");
+            ary = dht_str.split(",");
+            temp_part = ary[0]
+            rh_part = ary[1]
+            ary = temp_part.split(":");
+            temperature_f = float( ary[1] );
+            ary = rh_part.split(":");
+            humidity = float( ary[1] );
+            #print("Temperature=%0.3f, RH=%0.3f" % (temperature_f, humidity) )
+            #print(
+            #    "Temp: {:.1f} F /     Humidity: {}% ".format(
+            #        temperature_f, humidity
+            #    )
+            #)
+            now=datetime.today()
+            dt_string = now.strftime("%m/%d/%Y %H:%M:%S")
+            print("[" + dt_string + "] thermo: got temp=%0.1f humidity=%0.1f" % (temperature_f, humidity) );
+            now=datetime.today()
+            dt_string = now.strftime("%m/%d/%Y %H:%M:%S")
+            print("[" + dt_string + "] thermo: publishing topics loop # " + str(loop) );
+            publish_raw("thermostat/humidity", "{0:.1f}".format(humidity), printit);
+            publish_raw("thermostat/temperature", "{0:.1f}".format(temperature_f), printit);
+            publish_raw("$stats/lastupdate", dt_string, printit)
+            publish_raw("$stats/loop", str(loop), printit)
+            publish_raw("$state","ready",True,will);
+
+            result = 1
+            time.sleep(10)
+        else:
+            print("failed to get reading, trying again");
+    else:   
+#    except:
+        print('Failed to get reading. Try again!')
+
+        print("Unexpected error:", sys.exc_info()[0])
+        print("*** Exception, trying again, try %d ****" % retry);
+        time.sleep(1)
+
+        pass
+
+
+
