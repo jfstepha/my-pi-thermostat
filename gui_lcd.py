7e51a6b226839f6ea1a86fd6e31d529913549dee
Jon Stephan <jfstepha@gmail.com>
2021-08-26 09:12:51 -0400

    Changed the gui_lcd to use local dht values instead of mqtt

--- gui_lcd.py
+++ gui_lcd.py
@@ -1,21 +1,13 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 
 import sys
 import time
 import os
-import ConfigParser
 
-import paho.mqtt.client as mqtt
 from datetime import datetime
 
 import I2C_LCD_driver
-from time import *
-
-config = ConfigParser.ConfigParser()
-config.read("config.txt")
-did = config.get('homie','did')
-
 
 mylcd = I2C_LCD_driver.lcd()
 mylcd.lcd_display_string("Starting up..", 1)
@@ -25,36 +17,26 @@ rh=""
 result = None
 retry = 0
 
-# MQTT settings
-MQTT_HOST="basement-pc"
-MQTT_PORT=1883
-DEBUG=False
-loop=0
+while 1:
+	stream = os.popen('./read_dht22.py')
+	dht_str = stream.read()
+	now=datetime.today()
+	dt_string = now.strftime("%m/%d %I:%M %p")
+	print("[" + dt_string + "]" + "  Got DHT string: " + dht_str);
+	# string should be "temp:74.120,rh:41.500"
+	if dht_str.find("temp:") == 0:
+	    ary = dht_str.split(",");
+	    temp_part = ary[0]
+	    rh_part = ary[1]
+	    ary = temp_part.split(":");
+	    temperature_f = float( ary[1] );
+	    ary = rh_part.split(":");
+	    humidity = float( ary[1] );
 
-def on_connect(client, userdata, flags, rc):  # The callback for when the client connects to the broker
-    print("Connected with result code {0}".format(str(rc)))  # Print result of connection attempt
-    client.subscribe( "homie/" + did + "/thermostat/temperature")
-    client.subscribe( "homie/" + did + "/thermostat/humidity")
 
+	now=datetime.today()
+	dt_string = now.strftime("%m/%d %I:%M %p")
+	mylcd.lcd_display_string(dt_string, 1)
+	mylcd.lcd_display_string("%sF %s%%" % (temperature_f, humidity), 2)
+	time.sleep(10)
 
-def on_message(client, userdata, msg):  # The callback for when a PUBLISH message is received from the server.
-
-    print("Message received-> " + msg.topic + " " + str(msg.payload))  # Print a received msg
-    global temp
-    global rh
-    if msg.topic ==  "homie/" + did + "/thermostat/temperature" :
-        print("setting temp to " + str(msg.payload) )
-        temp = msg.payload
-    if msg.topic ==  "homie/" + did + "/thermostat/humidity" :
-        print("setting humidity to " + str(msg.payload) )
-        rh = msg.payload
-    now=datetime.today()
-    dt_string = now.strftime("%m/%d %I:%M %p")
-    mylcd.lcd_display_string(dt_string, 1)
-    mylcd.lcd_display_string("%sF %s%%" % (temp, rh), 2)
-
-client = mqtt.Client( did + "-gui")
-client.on_connect = on_connect  # Define callback function for successful connection
-client.on_message = on_message  # Define callback function for receipt of a message
-client.connect( MQTT_HOST, MQTT_PORT )
-client.loop_forever()  # Start networking daemon
